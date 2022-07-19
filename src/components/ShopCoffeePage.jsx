a987581e615049eaaf1becc89c4d82602d0d8994
Barkatulla <barkatullakhan71@gmail.com>
2022-07-19 16:33:12 +0530

    fixed some className issues and added routing functionality

---
+++ src/components/ShopCoffeePage.jsx
@@ -0,0 +1,11 @@
+
+
+const ShopCoffeePage = () => {
+    return ( 
+        <div className="container">
+            <h1 className="text-center">This is shop coffee page</h1>
+        </div>
+     );
+}
+ 
+export default ShopCoffeePage;
